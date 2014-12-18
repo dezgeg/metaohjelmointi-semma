0d18c31d89a00d34e8ebf849a37b1c80d0173717
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2014-12-18 05:03:59 +0200

    Explained makeZip

--- codes/zip-generator/MakeZip.hs
+++ codes/zip-generator/MakeZip.hs
@@ -6,16 +6,16 @@ import Language.Haskell.TH.Syntax
 makeZip :: Int -> Q Dec
 makeZip arity = let
   functionName = mkName (if arity == 2 then "zip" else "zip" ++ show arity)
-  varNames = map (\i -> mkName ("l" ++ show i)) [1..arity]
-
-  isEmptyExprs = listE (map (\v -> [| null $(varE v) |]) varNames)
-  tupleExpr = tupE (map (\v -> [| head $(varE v) |]) varNames)
-  recursiveCallExpr = appsE (varE functionName :
-                                map (\v -> [| tail $(varE v) |]) varNames)
+  argNames = map (\i -> mkName ("l" ++ show i)) [1..arity]
 
   body = runQ [| if or $(isEmptyExprs)
                     then []
                     else $(tupleExpr) : $(recursiveCallExpr) |]
 
-  clauses = [clause (map varP varNames) (normalB body) []]
+  isEmptyExprs = listE (map (\v -> [| null $(varE v) |]) argNames)
+  tupleExpr = tupE (map (\v -> [| head $(varE v) |]) argNames)
+  recursiveCallExpr = appsE (varE functionName :
+                                map (\v -> [| tail $(varE v) |]) argNames)
+
+  clauses = [clause (map varP argNames) (normalB body) []]
       in runQ (funD functionName clauses)
