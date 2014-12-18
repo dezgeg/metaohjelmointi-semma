f529f9edb73a404e7dd46a16383517cc29f67e61
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2014-12-18 07:48:24 +0200

    Final.

--- codes/Parsec.hs
+++ codes/Parsec.hs
@@ -1,8 +1,10 @@
 import Text.Parsec
 
-javaIdentifier = (letter <|> char '_') >> (many (letter <|> char '_' <|> digit))
+javaIdentifier = (letter <|> char '_') >>
+                 (many (letter <|> char '_' <|> digit))
 
 javaInteger = try hexNumber <|> octalNumber <|> decimalNumber
-    where hexNumber     = char '0' >> oneOf "xX" >> many (oneOf "0123456789abcdefABCDEF")
+    where hexNumber     = char '0' >> oneOf "xX" >>
+                          many (oneOf "0123456789abcdefABCDEF")
           octalNumber   = char '0' >> many (oneOf "01234567")
           decimalNumber = oneOf "123456789" >> many digit
